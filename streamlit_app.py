2c38e670d2ca424e6f12df558ccb08e159c5a663
cksir <netwong@gmail.com>
2023-11-25 22:58:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,5 @@
-import streamlit as st
-import mediapipe as mp
 import cv2
-import numpy as np
-import tempfile
-import time
-from PIL import Image
+import streamlit as st
 
-uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
-frame_skip = 300 # display every 300 frames
-
-if uploaded_video is not None: # run only when user uploads video
-    vid = uploaded_video.name
-    with open(vid, mode='wb') as f:
-        f.write(uploaded_video.read()) # save video to disk
-
-    st.markdown(f"""
-    ### Files
-    - {vid}
-    """,
-    unsafe_allow_html=True) # display file name
-
-    vidcap = cv2.VideoCapture(vid) # load video from disk
-    cur_frame = 0
-    success = True
-
-    while success:
-        success, frame = vidcap.read() # get next frame from video
-        if cur_frame % frame_skip == 0: # only analyze every n=300 frames
-            print('frame: {}'.format(cur_frame)) 
-            pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
-            st.image(pil_img)
-        cur_frame += 1
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
